f58818ea30addb8ba269f8692d54cbfef425293a
Sam <stieman@student.scalda.nl>
2020-09-04 14:59:00 +0200

    init

---
+++ projecten-plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: My event organisator plugin
+* Plugin URI: samtieman.com
+* Description: This plugin will help organising projects on your website
+* Author: Samuel W.R. Tieman
+* Author URI: samtieman.com
+* Version: 0.0.1
+* Text Domain: project-plugin
+* Domain Path: languages
+*
+* This is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with your plugin. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Define the plugin name:
+define ( 'PROJECTEN_PLUGIN', __FILE__ );
+
+?>
